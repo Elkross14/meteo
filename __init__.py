4856b2396d8c2e65ece006b14c4679c5f65fcc78
Pablo Durán <pabloduran1997@gmail.com>
2020-06-07 11:10:45 +0200

    vinculado con laravel

--- __init__.py
+++ __init__.py
@@ -1,29 +1,19 @@
 from Sensores import Sensores
-from GeneradorJson import GeneradorJson
 import time
-import shlex, subprocess
+import os
 
 datos = "sin datos"
 
 
 class Main:
-    while True:
-        sensores = Sensores()
-        temperaturaStr = sensores.getTemperature()
-        humedadStr = sensores.getHumidity()
-        fechaStr = sensores.getTimeNow()
+    sensores = Sensores()
+    temperaturaStr = sensores.getTemperature()
+    humedadStr = sensores.getHumidity()
+    fechaStr = sensores.getTimeNow()
 
-        generador = GeneradorJson(temperaturaStr, humedadStr, fechaStr)
-        generador.escribirJson()
+    link = "http://pabloduran.es/recibirdatos/{}/{}/{}".format(
+        temperaturaStr, humedadStr, fechaStr)
 
-        command_line = 'chromium-browser http://localhost:8082/'
-        args = shlex.split(command_line)
-        subprocess.run(args)
+    command_line = 'DISPLAY=:0 chromium-browser ' + link
 
-        time.sleep(3500.0)
-
-        command_line2 = 'pkill -o chromium'
-        args2 = shlex.split(command_line2)
-        subprocess.run(args2)
-
-        time.sleep(99.0)
+    os.system(command_line)
--- enviarDatos/leer.php
+++
@@ -1,9 +0,0 @@
-<?php
-$datos = file_get_contents("datos.json");
-$products = json_decode($datos, true);
-
-$objeto = array_chunk($products,3);
-$temperatura = $objeto[0][0];
-$humedad = $objeto[0][2];
-$fecha = $objeto[0][1];
-?>
